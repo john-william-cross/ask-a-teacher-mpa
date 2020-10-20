b34cb5a2d3ccad6eef3ca1e9a4664ea997312b22
John Cross <john.william.cross@gmail.com>
2020-10-20 12:46:01 -0700

    mock data added

---
+++ src/mock-data/non-teacher-user-schema.js
@@ -0,0 +1,5 @@
+{
+    id: "faccfe2e0004-5d127345-c95d-42c3-bd84faccfe2e0004",
+    email: "jay@hotmail.com",
+    createdAt: 1603203642,
+ },
---
+++ src/mock-data/submitted-questions-schema.js
@@ -0,0 +1,5 @@
+const submittedQuestionsSchema = [
+   {
+      question: String,
+   },
+];
