332d8a4c06075889abf65a25676af56e1cfccf1c
John Cross <john.william.cross@gmail.com>
2020-10-20 12:27:07 -0700

    add home state select for on teacher signup, begin creating mock data

--- src/component/pages/SignInSignUp.js
+++ src/component/pages/SignInSignUp.js
@@ -61,6 +61,62 @@ export default function SignInSignUp() {
                      <h1 className="text-center logo-text-font mb-6">
                         Thanks for joining us!
                      </h1>
+                     <p class="mt-2">Where do you teach?</p>
+
+                     <select class="col-5 form-control form-control-lg mb-5 mt-3">
+                        <option value="AL">Alabama</option>
+                        <option value="AK">Alaska</option>
+                        <option value="AZ">Arizona</option>
+                        <option value="AR">Arkansas</option>
+                        <option value="CA">California</option>
+                        <option value="CO">Colorado</option>
+                        <option value="CT">Connecticut</option>
+                        <option value="DE">Delaware</option>
+                        <option value="DC">District Of Columbia</option>
+                        <option value="FL">Florida</option>
+                        <option value="GA">Georgia</option>
+                        <option value="HI">Hawaii</option>
+                        <option value="ID">Idaho</option>
+                        <option value="IL">Illinois</option>
+                        <option value="IN">Indiana</option>
+                        <option value="IA">Iowa</option>
+                        <option value="KS">Kansas</option>
+                        <option value="KY">Kentucky</option>
+                        <option value="LA">Louisiana</option>
+                        <option value="ME">Maine</option>
+                        <option value="MD">Maryland</option>
+                        <option value="MA">Massachusetts</option>
+                        <option value="MI">Michigan</option>
+                        <option value="MN">Minnesota</option>
+                        <option value="MS">Mississippi</option>
+                        <option value="MO">Missouri</option>
+                        <option value="MT">Montana</option>
+                        <option value="NE">Nebraska</option>
+                        <option value="NV">Nevada</option>
+                        <option value="NH">New Hampshire</option>
+                        <option value="NJ">New Jersey</option>
+                        <option value="NM">New Mexico</option>
+                        <option value="NY">New York</option>
+                        <option value="NC">North Carolina</option>
+                        <option value="ND">North Dakota</option>
+                        <option value="OH">Ohio</option>
+                        <option value="OK">Oklahoma</option>
+                        <option value="OR">Oregon</option>
+                        <option value="PA">Pennsylvania</option>
+                        <option value="RI">Rhode Island</option>
+                        <option value="SC">South Carolina</option>
+                        <option value="SD">South Dakota</option>
+                        <option value="TN">Tennessee</option>
+                        <option value="TX">Texas</option>
+                        <option value="UT">Utah</option>
+                        <option value="VT">Vermont</option>
+                        <option value="VA">Virginia</option>
+                        <option value="WA">Washington</option>
+                        <option value="WV">West Virginia</option>
+                        <option value="WI">Wisconsin</option>
+                        <option value="WY">Wyoming</option>
+                     </select>
+
                      <p className="mt-2">Enter your CCSD email</p>
                      <input
                         id="sign-up-email-input"
---
+++ src/mock-data/non-teacher-users.js
@@ -0,0 +1,14 @@
+const nonTeacherUsers = [
+   {
+      id: "4f9811eade44-c653e41b-c585-4d27-a439",
+      email: "michelle@yahoo.com",
+      password: "1D8327DEB882CF995F4DCC3B5AA765D6",
+      createdAt: 1603203942,
+   },
+   {
+      id: "faccfe2e0004-5d127345-c95d-42c3-bd84faccfe2e0004",
+      email: "jay@hotmail.com",
+      password: "A30F996445FAEE84734F4C368A4AC071",
+      createdAt: 1603203642,
+   },
+];
