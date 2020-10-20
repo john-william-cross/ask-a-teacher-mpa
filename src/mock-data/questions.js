35339b6f4a6c33d5da74a094e012c78275f3b341
John Cross <john.william.cross@gmail.com>
2020-10-20 15:44:20 -0700

    enter realistic mock data

--- src/mock-data/questions.js
+++ src/mock-data/questions.js
@@ -1,34 +1,49 @@
-const text = 
+const text =
 `<p className="mb-4">"A prediction is an educated guess..."</p>, <p className="mb-4">"Heres an example..."</p>`,
 const openParagraphTag = <p className=" mb-4">{text}</p>;
 const questions = [
    {
-      id: "4f9811eade44-c653e41b-c585-4d27-a439",
+      questionId: "4f9811eade44-c653e41b-c585-4d27-a439",
       text: "What is the difference between an inference and a prediction?",
-      email: "michelle@yahoo.com",
+      email: "michelle@yahoo.com", //email associated with the question
       createdAt: 1603203942,
       answers: [
          {
-            id: "b37a480c-2de0-435e-8bc2-091760df2885", //this is the question
-            text:
-               `<p className="mb-4">"A prediction is an educated guess..."</p>, <p className="mb-4">"Heres an example..."</p>`,
+            answerId: "b37a480c-2de0-435e-8bc2-091760df2885", //this is the answer id
+            text: "A prediction is an educated guess, based on information you already have, about what you think will happen in the future. An inference is a conclusion you draw, based on information you already have, about what is happening NOW.\n Here’s some examples: The sky is quickly darkening, with swirling clouds and strong winds. You see lightning and hear thunder in the distance. Based on that information, you can predict that it is going to rain. Meanwhile, a family member is asleep inside your house. It rains, and they sleep through the entire storm. They wake up and look outside. There are puddles everywhere and everything is wet. From that information, they can infer that it rained.\nHere are some more inferences and predictions: A little kid’s bedroom has pink walls, pink carpet, a pink bedspread, and pink picture frames on the walls. From this information, you can infer that the kid likes the color pink.\nYou wake up with a stuffed nose and a sore throat. You sneeze all day. You might infer that you have a cold. Your birthday party is tomorrow. You’ve been asking for a bike. As you open up presents, the first gift you open up is a bike helmet. You can predict that one of your gifts will also be a bike.\nThis question was answered by a teacher in Iowa on September 4, 2020.",
             answeredAt: 1603232091,
             userId: "c653e41b-c585-4d27-a439-4f9811eade44",
-            userHomeState: String,
+            userHomeState: "Iowa",
          },
          {
-            id: String,
-            text: "Predictions are guesses about the future based on current evidence. Inferences are conclusions drawn about the current time. Think of a detective. A detective looks at clues and makes an inference about what might have happened at a crime scene. The detective infers what happened at the crime scene, meaning she used clues to guess what already happened.",
-            answeredAt: Number,
-            userId: String,
-            userHomeState: String,
+            answerId: "3681497b-e639-42eb-b7cf-00af98ebd64d",
+            text: "Predictions are guesses about the future based on current evidence. Inferences are conclusions drawn about the current time. Think of a detective. A detective looks at clues and makes an inference about what might have happened at a crime scene. The detective infers what happened at the crime scene, meaning she used clues to guess what already happened.\nThis question was answered by a teacher in Nevada on May 2, 1984.",
+            answeredAt: 1603232097,
+            userId: "5d127345-c95d-42c3-bd84-faccfe2e0004",
+            userHomeState: "Nevada",
          },
       ],
    },
    {
-      id: "faccfe2e0004-5d127345-c95d-42c3-bd84faccfe2e0004",
-      text: "how do i math?",
+      questionId: "30a95c2b-ed3c-46a4-b32f-7bcb5aef7143", 
+      text: "I don’t know anything about common core my child is having trouble with math and I don’t know how he’s supposed to do it he says I’m not showing him the right way please help",
       email: "jay@hotmail.com",
-      createdAt: 1603203642,
+      createdAt: 1603232099,
+      answers: [
+         {
+            answerId:"c69631b0-bf11-41c2-86b9-5e3c0c8ab182", 
+            text: "You may need to submit a new question in which you say what grade your child is in, because different methods for solving different types of problems are introduced at different grades.",
+            answeredAt: 1603232061,
+            userId: "c653e41b-c585-4d27-a439-4f9811eade44",
+            userHomeState: "Iowa",
+         },
+         {
+            answerId: "6423a37b-459e-4376-93d4-e0535bbbe9c5",
+            text: "If it is multiplication, please google `area model multiplication common core` and you should find some good examples",
+            answeredAt: 1603200061,
+            userId:  "5d127345-c95d-42c3-bd84-faccfe2e0004",
+            userHomeState: "Nevada",
+         },
+      ],
    },
 ];
