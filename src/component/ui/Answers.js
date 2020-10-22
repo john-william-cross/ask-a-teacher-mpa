b847b28929bc52198806af6ac33899387e35ec43
John Cross <john.william.cross@gmail.com>
2020-10-22 10:10:27 -0700

    changes on mike's suggestiosn

--- src/component/ui/Answers.js
+++ src/component/ui/Answers.js
@@ -2,28 +2,24 @@ import React from "react";
 import Header from "../ui/Header";
 import questions from "../../mock-data/questions";
 import toDisplayDate from "date-fns/format";
-
-const answers = questions[0].answers; // map through all answers
-// create answers component
-// map over all answers on this page
-// render an answer component for each answer
-const answeredAtDate = answers[0].answeredAt;
+const question = questions[0];
+const answers = question.answers;
 
 export default function Answer() {
    return (
       <>
          <div className="clearfix mb-4"></div>
          <div className="answers" style={{ textAlign: "justify" }}>
-            {questions[0].answers.map((answer) => {
+            {answers.map((answer) => {
+               console.log(answer);
                return (
                   <div className="answers">
                      <p className="mb-4">
-                        {" "}
                         {answer.text}
                         <span className="text-muted">
                            This question was answered by a teacher in{" "}
-                           {answers[0].userHomeState} on{" "}
-                           {toDisplayDate(answeredAtDate, "MMM. d, y")}.
+                           {answer.userHomeState} on{" "}
+                           {toDisplayDate(answer.answeredAt, "MMM. d, y")}.
                         </span>
                      </p>
                   </div>
