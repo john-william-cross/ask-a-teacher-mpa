2b25bc589162e92ad341c09fa40fb014e4c7cfd0
John Cross <john.william.cross@gmail.com>
2020-10-21 17:49:31 -0700

    more progress with iterating over data, some formatting

--- src/component/pages/Question.js
+++ src/component/pages/Question.js
@@ -17,57 +17,60 @@ export default function Question() {
          <Header />
          <div className="container">
             <div className="row no-gutters">
-               <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2 mt-9">
-                  <div className="lead">
-                     <Link to="question">
-                        What is the difference between an inference and a
-                        prediction?
-                     </Link>
-                  </div>
-                  <p className="text-muted asked-on-answers-num float-left">
-                     Asked on June 1, 2020
-                  </p>
-                  <p className="text-muted asked-on-answers-num float-right">
-                     {answers.length} answers
-                  </p>
-                  <div className="clearfix"></div>
-                  {/* {questions.map((question) => {
-                     return question.answers.map((answer) => {
-                        return <Answers answer={answer.text} />;
-                     });
-                  })} */}{" "}
-                  {questions[0].answers.map((answer) => {
-                     return answer.text;
-                  })}
-                  {/* questions.map((question) => {
+               <div className="col-md-10 offset-md-1 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2 mt-9">
+                  <div className="col-12">
+                     <div className="lead mb-4">
+                        <Link to="question">
+                           What is the difference between an inference and a
+                           prediction?
+                        </Link>
+                     </div>
+                     <p className="text-muted asked-on-answers-num float-left">
+                        Asked on June 1, 2020
+                     </p>
+                     <p className="text-muted asked-on-answers-num float-right">
+                        {answers.length} answers
+                     </p>
+                     <div className="clearfix mb-4"></div>
+                     <div class="answers" style={{ textAlign: "justify" }}>
+                        {questions[0].answers.map((answer) => {
+                           return answer.text;
+                        })}
+                     </div>
+
+                     {/* questions.map((question) => {
                         question.answers.map((answer) => {
                         // do stuff to each answer
                        }
                       } */}
-                  <div className="clearfix mb-4"></div>
-                  <hr className="my-6" />
-                  <p className="lead mt-1">Your answer</p>
-                  <textarea
-                     className="form-control form-control-lg"
-                     id="answer-input"
-                     rows="10"
-                     style={{ width: "100%" }}
-                  ></textarea>
-                  <p className="float-right lead mt-0 text-muted">
-                     <span className="text-danger" id="answer-input-char-count">
-                        0
-                     </span>
-                     /2000
-                  </p>
-                  <button
-                     className="mt-5 mb-8 submit-answer-button logo-text-font btn btn-lg btn-outline-primary"
-                     id="submit-answer"
-                     // disabled
-                     type="submit"
-                     value="Submit answer"
-                  >
-                     Submit answer
-                  </button>
+                     <div className="clearfix mb-7"></div>
+                     <hr className="my-6" />
+                     <p className="lead mt-1">Your answer</p>
+                     <textarea
+                        className="form-control form-control-lg"
+                        id="answer-input"
+                        rows="10"
+                        style={{ width: "100%" }}
+                     ></textarea>
+                     <p className="float-right lead mt-0 text-muted">
+                        <span
+                           className="text-danger"
+                           id="answer-input-char-count"
+                        >
+                           0
+                        </span>
+                        /2000
+                     </p>
+                     <button
+                        className="mt-5 mb-8 submit-answer-button logo-text-font btn btn-lg btn-outline-primary"
+                        id="submit-answer"
+                        // disabled
+                        type="submit"
+                        value="Submit answer"
+                     >
+                        Submit answer
+                     </button>
+                  </div>
                </div>
             </div>
          </div>
