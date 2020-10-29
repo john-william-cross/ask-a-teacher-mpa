bf5555b28df893cebfc69df04eae0a8cc4e05de7
John Cross <john.william.cross@gmail.com>
2020-10-29 06:57:28 -0700

    commit at 46minutes into 320A Redux

--- src/store/combineReducers.js
+++ src/store/combineReducers.js
@@ -1,6 +1,11 @@
 import { combineReducers } from "redux";
+import currentUser from "./reducers/currentUser";
+import allQuestions from "./reducers/allQuestions";
 
-export default combineReducers({});
+export default combineReducers({ currentUser, allQuestions });
+
+//because the keys and values are the same, ie.e currentUser: currentUser, allQuestions: allQuestions; we can use shorthand and just list them like we do above.
+
 //combineReducers takes an object, which is basically key:value pairs of all the other reducers that we have
 
 //reducer is passed a state and an action. Based on that, it will return some new state. It will likely update the global state inside the redux store
