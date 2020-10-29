e175f65736e0d48036a5ee2bbecda1a83501a66d
John Cross <john.william.cross@gmail.com>
2020-10-29 09:54:03 -0700

    update url for current user api call

--- src/component/pages/LogIn.js
+++ src/component/pages/LogIn.js
@@ -15,7 +15,7 @@ class LogIn extends React.Component {
       super(props);
       axios
          .get(
-            "https://raw.githubusercontent.com/john-william-cross/ask-a-teacher-mpa/master/src/mock-data/users.json" //rewrite so it only returns one user, which should be currentUser.json
+            "https://github.com/john-william-cross/ask-a-teacher-mpa/blob/63a7c06d45373a0cd66b7ccb4f5dd20b5107312c/src/mock-data/currentUser.json" //rewrite so it only returns one user, which should be currentUser.json
          )
          .then(function (res) {
             // handle success
