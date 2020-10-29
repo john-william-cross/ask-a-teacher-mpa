9530d116e7beb3824c4dc0a88e36572c97827e10
John Cross <john.william.cross@gmail.com>
2020-10-29 13:05:57 -0700

    change textarea to input, some refactoring. at 12:25 of 321A refactoring redux

--- src/store/reducers/currentUser.js
+++ src/store/reducers/currentUser.js
@@ -2,13 +2,13 @@
 //based on the action, it creates a new state
 import actions from "../actions";
 
-export default function currentUser(state = {}, action) {
+export default function currentUser(currentUser = {}, action) {
    //action has two things associated with it: action.payload and action.type
    switch (action.type) {
       case actions.STORE_CURRENT_USER:
-         return action.payload;
+         return action.payload; //what's the action payload in Login? it's res.data- our data object that contains currentUser data.
       default:
-         return state;
+         return currentUser; //this was called 'state', but we know it's the state of the current user, so we'll call it currentUser
    }
 }
 //default state of currentUser is that its an empty object
