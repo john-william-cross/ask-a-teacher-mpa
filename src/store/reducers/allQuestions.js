664a4abc07f1f22cf6d366d8451cb597fca5e3e7
John Cross <john.william.cross@gmail.com>
2020-10-30 10:52:51 -0700

    commit at 53:18 of 320A repeat

---
+++ src/store/reducers/allQuestions.js
@@ -0,0 +1,11 @@
+import actions from "../actions";
+
+export default function allQuestions(state = [], action) {
+   // action.payload, action.type
+   switch (action.type) {
+      case actions.STORE_ALL_QUESTION:
+         return action.payload; //this will be an array of objects that will be stored inside our Redux store
+      default:
+         return state;
+   }
+}
--- src/store/reducers/currentUser.js
+++ src/store/reducers/currentUser.js
@@ -1,13 +1,13 @@
 //give it the action, and the previous state.
 //based on the action, it creates a new state
+
 import actions from "../actions";
 
 export default function currentUser(state = {}, action) {
    switch (action.type) {
       case actions.STORE_CURRENT_USER:
-         return {};
+         return action.payload;
       default:
          return state;
    }
 }
-//default state of currentUser is that its an empty object
