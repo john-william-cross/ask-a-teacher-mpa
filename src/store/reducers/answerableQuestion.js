fafa2b14961f70e29c90a1f12a766cdb96fca1a1
John Cross <john.william.cross@gmail.com>
2020-11-02 09:55:25 -0800

    console logged newAnswerableQuestion

--- src/store/reducers/answerableQuestion.js
+++ src/store/reducers/answerableQuestion.js
@@ -13,6 +13,10 @@ export default function answerableQuestion(answerableQuestion = {}, action) {
          newAnswerableQuestion.text = action.payload.text;
          newAnswerableQuestion.email = action.payload.email;
          newAnswerableQuestion.createdAt = action.payload.createdAt;
+         console.log(
+            "here's the newAnswerableQuestion: ",
+            newAnswerableQuestion
+         );
 
          return newAnswerableQuestion; //this will be an array of objects that will be stored inside our Redux store
       default:
