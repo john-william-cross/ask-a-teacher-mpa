ecadf713b292a397f3b55ca4cace06e9a38f3fdc
John Cross <john.william.cross@gmail.com>
2020-11-03 13:47:30 -0800

    working on contact margin in footer

--- src/component/ui/Footer.js
+++ src/component/ui/Footer.js
@@ -3,28 +3,27 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
    return (
-      <div className="footer-solid fixed-bottom">
-         <div className="container mb-4 mt-4 mt-sm-4 mt-md-3 mt-lg-2">
-            <div className="row">
-               <div className="col-6">
-                  <Link
-                     to="/LogIn"
-                     className="no-underline text-right answer-question-link"
-                  >
-                     Contact
-                  </Link>
-               </div>
-               <div className="col-6">
-                  <Link
-                     to="/LogIn"
-                     className="float-right mr-1 my-3 no-underline text-right answer-question-link"
-                  >
-                     Sign out
-                  </Link>
+      <>
+         <div className="footer-solid fixed-bottom">
+            <div className="container mb-4 mt-4 mt-sm-4 mt-md-3 mt-lg-2">
+               <div className="row">
+                  <div className="col-6 my-3">
+                     <Link to="/LogIn" className="contact no-underline">
+                        Contact
+                     </Link>
+                  </div>
+                  <div className="col-6">
+                     <Link
+                        to="/LogIn"
+                        className="sign-out float-right my-3 no-underline text-right"
+                     >
+                        Sign out
+                     </Link>
+                  </div>
                </div>
             </div>
          </div>
-      </div>
+      </>
    );
 }
 
