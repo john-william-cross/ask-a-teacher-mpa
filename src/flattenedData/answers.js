94975f9901ed1d987715d1f656d847ccb5930ba9
John Cross <john.william.cross@gmail.com>
2020-11-04 18:35:19 -0800

    flattened questions

---
+++ src/flattenedData/answers.js
@@ -0,0 +1,48 @@
+const { map } = require("lodash");
+
+const answers = [
+   {
+      id: "b37a480c-2de0-435e-8bc2-091760df2885",
+      text:
+         "A prediction is an educated guess, based on information you already have, about what you think will happen in the future. An inference is a conclusion you draw, based on information you already have, about what is happening NOW.\n\nHere are some examples: The sky is quickly darkening, with swirling clouds and strong winds. You see lightning and hear thunder in the distance. Based on that information, you can predict that it is going to rain. Meanwhile, a family member is asleep inside your house. It rains, and they sleep through the entire storm. They wake up and look outside. There are puddles everywhere and everything is wet. From that information, they can infer that it rained.\n\nHere are some more inferences and predictions: A little kid’s bedroom has pink walls, pink carpet, a pink bedspread, and pink picture frames on the walls. From this information, you can infer that the kid likes the color pink.\n\nYou wake up with a stuffed nose and a sore throat. You sneeze all day. You might infer that you have a cold. Your birthday party is tomorrow. You’ve been asking for a bike. As you open up presents, the first gift you open up is a bike helmet. You can predict that one of your gifts will also be a bike.\n\n",
+      answeredAt: 1603329563286,
+      userId: "5d127345-c95d-42c3-bd84-faccfe2e0004",
+      userHomeState: "Iowa",
+      questionId: "4f9811eade44-c653e41b-c585-4d27-a439",
+   },
+   {
+      id: "3681497b-e639-42eb-b7cf-00af98ebd64d",
+      text:
+         "Predictions are guesses about the future based on current evidence. Inferences are conclusions drawn about the current time. Think of a detective. A detective looks at clues and makes an inference about what might have happened at a crime scene. The detective infers what happened at the crime scene, meaning she used clues to guess what already happened.\n\n",
+      answeredAt: 1500380071259,
+      userId: "5d127345-c95d-42c3-bd84-faccfe2e0004",
+      userHomeState: "Iowa",
+      questionId: "4f9811eade44-c653e41b-c585-4d27-a439",
+   },
+   {
+      id: "c69631b0-bf11-41c2-86b9-5e3c0c8ab182",
+      text:
+         "You may need to submit a new question in which you say what grade your child is in, because different methods for solving multiplication problems are introduced at different grades.\n\n",
+      answeredAt: 1600650071259,
+      userId: "5d127345-c95d-42c3-bd84-faccfe2e0004",
+      userHomeState: "Iowa",
+      questionId: "30a95c2b-ed3c-46a4-b32f-7bcb5aef7723",
+   },
+   {
+      id: "6423a37b-459e-4376-93d4-e0535bbbe9c5",
+      text:
+         "If it is multiplication, please google `area model multiplication common core` and you should find some good examples\n\n",
+      answeredAt: 1609380071259,
+      userId: "5d127345-c95d-42c3-bd84-faccfe2e0004",
+      userHomeState: "Iowa",
+      questionId: "30a95c2b-ed3c-46a4-b32f-7bcb5aef7723",
+   },
+   {
+      id: "6423a37b-459e-4376-93d4-e0535bbbe9c1",
+      text: "Look up `multiplication with arrays`, that may help!\n\n",
+      answeredAt: 1609380071259,
+      userId: "5d127345-c95d-42c3-bd84-faccfe2e0004",
+      userHomeState: "Iowa",
+      questionId: "30a95c2b-ed3c-46a4-b32f-7bcb5aef7723",
+   },
+];
