db3da9e6f4a65dc9877b71d0004d2153586e8f0a
John Cross <john.william.cross@gmail.com>
2020-11-05 09:53:36 -0800

    commit before removing bridge table data

--- src/flattenedData/user-answers.js
+++ src/flattenedData/user-answers.js
@@ -2,43 +2,43 @@ const userAnswers = [
    {
       id: "8e5bf525-7fc5-49eb-8fcd-90057088ea0a",
       userId: "5d127345-c95d-42c3-bd84-faccfe2e0004",
-      questionId: "4f9811eade44-c653e41b-c585-4d27-a439",
-      userAnswerId: "b37a480c-2de0-435e-8bc2-091760df2885", // id of answer user submitted
-      userAnsweredAt: 1603329563286, //time answered
+      //   questionId: "4f9811eade44-c653e41b-c585-4d27-a439", don't need these
+      answerId: "b37a480c-2de0-435e-8bc2-091760df2885", // id of answer user submitted
+      //   userAnsweredAt: 1603329563286, //time answered
    },
    {
       id: "2067641f-1685-4a9c-af17-6ed85d06578a",
       userId: "5d127345-c95d-42c3-bd84-faccfe2e0004",
-      questionId: "4f9811eade44-c653e41b-c585-4d27-a439",
-      userAnswerId: "3681497b-e639-42eb-b7cf-00af98ebd64d", // id of answer user submitted
-      userAnsweredAt: 1500380071259, //time answered
+      //   questionId: "4f9811eade44-c653e41b-c585-4d27-a439",
+      answerId: "3681497b-e639-42eb-b7cf-00af98ebd64d", // id of answer user submitted
+      //   userAnsweredAt: 1500380071259, //time answered
    },
    {
       id: "d7a17b29-6f07-4d98-a09b-7f56239f1dd3",
       userId: "5d127345-c95d-42c3-bd84-faccfe2e0004",
-      questionId: "30a95c2b-ed3c-46a4-b32f-7bcb5aef7723",
-      userAnswerId: "c69631b0-bf11-41c2-86b9-5e3c0c8ab182", // id of answer user submitted
-      userAnsweredAt: 1600650071259, //time answered
+      //   questionId: "30a95c2b-ed3c-46a4-b32f-7bcb5aef7723",
+      answerId: "c69631b0-bf11-41c2-86b9-5e3c0c8ab182", // id of answer user submitted
+      //   userAnsweredAt: 1600650071259, //time answered
    },
    {
       id: "62a30eda-ce68-43c2-bc4d-6e05f447029a",
       userId: "5d127345-c95d-42c3-bd84-faccfe2e0004",
-      questionId: "30a95c2b-ed3c-46a4-b32f-7bcb5aef7723",
-      userAnswerId: "6423a37b-459e-4376-93d4-e0535bbbe9c5", //id of answer user submitted
-      userAnsweredAt: 1600650071259, //time answered
+      //   questionId: "30a95c2b-ed3c-46a4-b32f-7bcb5aef7723",
+      answerId: "6423a37b-459e-4376-93d4-e0535bbbe9c5", //id of answer user submitted
+      //   userAnsweredAt: 1600650071259, //time answered
    },
    {
       id: "695744a0-cee5-4551-9c7a-a0e118b35ad3",
       userId: "5d127345-c95d-42c3-bd84-faccfe2e0004",
-      questionId: "30a95c2b-ed3c-46a4-b32f-7bcb5aef7723",
-      userAnswerId: "6423a37b-459e-4376-93d4-e0535bbbe9c1", //id of answer user submitted
-      userAnsweredAt: 1609380071259, //time answered
+      //   questionId: "30a95c2b-ed3c-46a4-b32f-7bcb5aef7723",
+      answerId: "6423a37b-459e-4376-93d4-e0535bbbe9c1", //id of answer user submitted
+      //   userAnsweredAt: 1609380071259, //time answered
    },
    {
       id: "da34553e-4af4-4d8f-bc72-ae96bd15e78b",
       userId: "35f222eb-23f8-4598-bb7b-7c5faed30292",
-      questionId: "30a95c2b-ed3c-46a4-b32f-7bcb5aef7143",
-      userAnswerId: "dbe66104-0335-42c9-8e23-e45eef4161e1", //id of answer user submitted
-      userAnsweredAt: 1604544431124, //time answered
+      //   questionId: "30a95c2b-ed3c-46a4-b32f-7bcb5aef7143",
+      answerId: "dbe66104-0335-42c9-8e23-e45eef4161e1", //id of answer user submitted
+      //   userAnsweredAt: 1604544431124, //time answered
    },
 ];
