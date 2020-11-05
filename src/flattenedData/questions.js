285ef1733c6ae0c5f8bf66712aa50bf91a354601
John Cross <john.william.cross@gmail.com>
2020-11-04 18:15:43 -0800

    begin to flatten data

---
+++ src/flattenedData/questions.js
@@ -0,0 +1,20 @@
+const questions = [
+   {
+      id: "4f9811eade44-c653e41b-c585-4d27-a439",
+      text: "What is the difference between an inference and a prediction?",
+      email: "michelle@yahoo.com",
+      createdAt: 1603382271259,
+   },
+   {
+      id: "30a95c2b-ed3c-46a4-b32f-7bcb5aef7723",
+      text: "What is the area model of multiplication?",
+      email: "jay@hotmail.com",
+      createdAt: 1600380071259,
+   },
+   {
+      id: "30a95c2b-ed3c-46a4-b32f-7bcb5aef7143",
+      text: "How do I help my kindergarten learn to rhyme?",
+      email: "tony@hotmail.com",
+      createdAt: 1600390071259,
+   },
+];
